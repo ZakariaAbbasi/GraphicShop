f6d92e60fdf62d5136e7a4cb5b86585054a5f1b5
Zakaria Abbasi <zakaria_abbasi@yahoo.com>
2022-02-01 23:03:04 +0330

    users-table-completed

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,9 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->char('mobile', 11)->nullable();
             $table->string('password');
+            $table->enum('role', ['admin', 'user']);
             $table->rememberToken();
             $table->timestamps();
         });
